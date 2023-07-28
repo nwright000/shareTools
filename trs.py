5d5e21d584608925cadf755ea92006ba362f34df
Ned Wright <ned.wright.000@gmail.com>
2023-07-28 15:10:50 -0500

    Update trs.py
    Updated to most recent version....

--- trs.py
+++ trs.py
@@ -7,14 +7,15 @@ def zoom_to_plss_location(feature_class, state_field, township_field, range_fiel
     # Create a feature layer to perform the selection
     selectFL = "selectedFeatures"
     arcpy.MakeFeatureLayer_management(feature_class, selectFL, query)
-    result = arcpy.management.GetCount(selectFL)
-    if int(result.getOutput(0)) != 0:
-        print('shitThere')
-    else:
-        print('noSuchPlace')
-    outFC = r"C:\temp\temp.shp"
+    outLoc = arcpy.GetParameterAsText(4)
+    outFC = outLoc + r"\temp.shp"
+    if not os.path.exists(outLoc):
+        os.makedirs(outLoc)
+    arcpy.management.CopyFeatures(selectFL, outFC)
     arcpy.analysis.Buffer(selectFL, outFC, ".25 Miles")
-    arcpy.management.RecalculateFeatureClassExtent(outFC)
+    if int(arcpy.GetCount_management(outFC)[0]) == 0:
+        arcpy.AddError("This legal description is not in this database.")
+        raise arcpy.ExecuteError
 
     # Get the extent of the selected features
     extent = arcpy.Describe(outFC).extent
@@ -44,10 +45,10 @@ section_field = "SECTION"
 
 # Replace 'TargetState', 'TargetTownship', 'TargetRange', and 'TargetSection' with the specific
 # state, township, range, and section values you want to zoom to
-target_state = "ND"
-target_township = 131
-target_range = 69
-target_section = 24
+target_state = arcpy.GetParameterAsText(0)
+target_township = arcpy.GetParameterAsText(1)
+target_range = arcpy.GetParameterAsText(2)
+target_section = arcpy.GetParameterAsText(3)
 
 # Call the function to zoom to the specified PLSS location
 zoom_to_plss_location(feature_class, state_field, township_field, range_field, section_field,
